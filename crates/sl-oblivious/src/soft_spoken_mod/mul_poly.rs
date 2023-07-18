9fa479af7c1ebeb9ad6cc3afe2dc75f9a8a7ef21
Vladyslav Khomenko <khomenko.vladyslav7@gmail.com>
2023-07-18 17:54:11 +0300

    Constant time in binary_field_multiply_gf_2_128()

--- crates/sl-oblivious/src/soft_spoken_mod/mul_poly.rs
+++ crates/sl-oblivious/src/soft_spoken_mod/mul_poly.rs
@@ -5,44 +5,38 @@
 ///     multiplication part: Algorithm 2.34, "Right-to-left comb method for polynomial multiplication"
 ///     reduction part: variant of the idea of Figure 2.9
 pub fn binary_field_multiply_gf_2_128(a_data: &[u8; 16], b_data: &[u8; 16]) -> [u8; 16] {
-    let mut r = [0u8; 16];
-    for i in 0..16 {
-        r[i] = a_data[i] & b_data[i];
+    const W: usize = 8;
+    const T: usize = 16;
+    let mut c = [0u8; T * 2];
+    let mut a = [0u8; T];
+    let mut b = [0u8; T + 1];
+    a[..16].copy_from_slice(&a_data[..16]);
+    b[..16].copy_from_slice(&b_data[..16]);
+
+    for k in 0..W {
+        for j in 0..T {
+            //let mask = if (a[j] >> k) & 0x01 == 1 { 0xFF } else { 0x00 };
+            let mask = -((a[j] >> k & 0x01) as i8) as u8;
+            for i in 0..T + 1 {
+                c[j + i] ^= b[i] & mask;
+            }
+        }
+        for i in (1..=T).rev() {
+            b[i] = (b[i] << 1) | (b[i - 1] >> 7);
+        }
+        b[0] <<= 1
+    }
+    for i in (T..=2 * T - 1).rev() {
+        c[i - 16] ^= c[i];
+        c[i - 16] ^= c[i] << 1;
+        c[i - 15] ^= c[i] >> 7;
+        c[i - 16] ^= c[i] << 2;
+        c[i - 15] ^= c[i] >> 6;
+        c[i - 16] ^= c[i] << 7;
+        c[i - 15] ^= c[i] >> 1;
     }
 
-    r
-
-    // const W: usize = 8;
-    // const T: usize = 16;
-    // let mut c = [0u8; T * 2];
-    // let mut a = [0u8; T];
-    // let mut b = [0u8; T + 1];
-    // a[..16].copy_from_slice(&a_data[..16]);
-    // b[..16].copy_from_slice(&b_data[..16]);
-    //
-    // for k in 0..W {
-    //     for j in 0..T {
-    //         let mask = if (a[j] >> k) & 0x01 == 1 { 0xFF } else { 0x00 };
-    //         for i in 0..T + 1 {
-    //             c[j + i] ^= b[i] & mask;
-    //         }
-    //     }
-    //     for i in (1..=T).rev() {
-    //         b[i] = (b[i] << 1) | (b[i - 1] >> 7);
-    //     }
-    //     b[0] <<= 1
-    // }
-    // for i in (T..=2 * T - 1).rev() {
-    //     c[i - 16] ^= c[i];
-    //     c[i - 16] ^= c[i] << 1;
-    //     c[i - 15] ^= c[i] >> 7;
-    //     c[i - 16] ^= c[i] << 2;
-    //     c[i - 15] ^= c[i] >> 6;
-    //     c[i - 16] ^= c[i] << 7;
-    //     c[i - 15] ^= c[i] >> 1;
-    // }
-    //
-    // c[..16].try_into().unwrap()
+    c[..16].try_into().unwrap()
 }
 
 #[cfg(test)]
