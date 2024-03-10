35f9f3e31df48c49d51287319a83448a562edf3e
Sushi <sushi@silencelaboratories.com>
2024-03-10 16:28:19 +0400

    Add crate for common traits  (#3)
    * feat(sl-mpc-traits): add sl-mpc-traits

---
+++ crates/sl-mpc-traits/src/lib.rs
@@ -0,0 +1,28 @@
+/// Trait that defines a state transition for any round based protocol.
+pub trait Round {
+    /// Output of the state transition.
+    type Output;
+    /// Input of the state transition.
+    type Input;
+    /// Transition to the next state.
+    fn process(self, messages: Self::Input) -> Self::Output;
+}
+
+#[cfg(feature = "serde")]
+use serde::{de::DeserializeOwned, Serialize};
+
+#[cfg(feature = "serde")]
+/// Trait that signifies a persistent object.
+/// Encoded using bincode.
+pub trait PersistentObject:
+    Serialize + DeserializeOwned + Send + 'static
+{
+    /// Serialize the object into bytes.
+    fn to_bytes(&self) -> Option<Vec<u8>> {
+        bincode::serialize(self).ok()
+    }
+    /// Deserialize the object from bytes.
+    fn from_bytes(bytes: &[u8]) -> Option<Self> {
+        bincode::deserialize(bytes).ok()
+    }
+}
