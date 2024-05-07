cc4e0026f3738d8fb7883966f9cf2f5d3fa96d53
Artem Mironov <artem.mironov@gmail.com>
2024-05-07 10:39:35 +0200

    Remove 'static lifetime bound for Relay trait
    And give method new() to MaybeFeed

--- crates/sl-mpc-mate/src/coord.rs
+++ crates/sl-mpc-mate/src/coord.rs
@@ -29,7 +29,11 @@ pub struct MessageSendError;
 
 pub struct MaybeFeed<'a, S: ?Sized>(Option<Feed<'a, S, Vec<u8>>>);
 
-impl<Si: Sink<Vec<u8>> + Unpin> MaybeFeed<'_, Si> {
+impl<'a, Si: Sink<Vec<u8>> + Unpin> MaybeFeed<'a, Si> {
+    pub fn new(feed: Feed<'a, Si, Vec<u8>>) -> Self {
+        Self(Some(feed))
+    }
+
     pub fn skip() -> Self {
         Self(None)
     }
@@ -50,10 +54,7 @@ impl<Si: Sink<Vec<u8>> + Unpin> Future for MaybeFeed<'_, Si> {
 }
 
 pub trait Relay:
-    Stream<Item = Vec<u8>>
-    + Sink<Vec<u8>, Error = MessageSendError>
-    + Unpin
-    + 'static
+    Stream<Item = Vec<u8>> + Sink<Vec<u8>, Error = MessageSendError> + Unpin
 {
     fn ask(&mut self, id: &MsgId, ttl: u32) -> MaybeFeed<'_, Self> {
         MaybeFeed(Some(self.feed(AskMsg::allocate(id, ttl))))
