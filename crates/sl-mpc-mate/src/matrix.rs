fe47c11441ff9bbeeb6299fd8038044780744461
Artem Mironov <artem.mironov@gmail.com>
2025-02-03 11:18:06 +0100

    Remove unsafe code

--- crates/sl-mpc-mate/src/matrix.rs
+++ crates/sl-mpc-mate/src/matrix.rs
@@ -34,12 +34,14 @@ fn transpose<C: CurveArithmetic>(
     let len = v[0].len();
 
     for n in 0..len - 1 {
-        for m in n + 1..len {
-            let nm = &mut v[n][m] as *mut C::Scalar;
-            let mn = &mut v[m][n] as *mut C::Scalar;
+        // n < len-1 => n+1 < len
+        let (up, down) = v.split_at_mut(n + 1);
 
-            // SAFETY: n != m, so nm and mn always points to separate locations
-            unsafe { core::ptr::swap_nonoverlapping(nm, mn, 1) }
+        for m in n + 1..len {
+            let nm = &mut up[n][m]; // == &mut v[n][m]
+            let mn = &mut down[m - (n + 1)][n]; // == &mut v[m][n]
+
+            core::mem::swap(nm, mn);
         }
     }
 
